4b78f1af2859b879c7da78ee00c65377f5cf190a
Calvin Dell'Oro <calvindelloro@mail.dem>
2022-04-26 16:40:40 +0200

    init + setup(scene): implemented rotating cube according to deltatime

---
+++ main.js
@@ -0,0 +1,37 @@
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+console.log("hello")
+
+let now = 0
+
+let deltaTime = 0
+
+function calculateDeltaTime(){
+    deltaTime = (Date.now() - now) / 1000
+    now = Date.now()
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    calculateDeltaTime();
+
+    cube.rotateY(1 * deltaTime)
+
+    renderer.render(scene, camera);
+}
+animate();

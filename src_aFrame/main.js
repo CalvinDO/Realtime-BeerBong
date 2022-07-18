8a1792c731240e190906421560e181725aad20ab
lukas <willmann.Lukas@web.de>
2022-07-18 19:15:03 +0200

    included drunk shader for ball and added some comments

--- src_aFrame/main.js
+++ src_aFrame/main.js
@@ -8,6 +8,10 @@ import { Camera } from './Camera.js';
 import { Scene } from './Scene.js';
 import { ScaleEntity } from './ScaleEntity.js';
 
+
+///////////////////////////////////////////////////////////////////////
+/*///////////////////////////// Global Variables//////////////////// */
+///////////////////////////////////////////////////////////////////////
 let ball;
 let renderer;
 let camera;
@@ -17,13 +21,17 @@ const loader = new GLTFLoader();
 let orbitControls = false;
 let ballThrow = true;
 
-let scale = 4.5;
+
+let scale = 4.5; // fitting for our Model to get realistic sizes 
 
 if (ballThrow) {
     window.addEventListener("touchstart", onTouchStart);
     window.addEventListener("touchend", onTouchEnd);
 }
 
+let startTime = Date.now() // used for the shader to get the time since start
+let drunk = true;
+let drunkStage = 0.9;
 
 
 let gravity = new THREE.Vector3(0, -9.81, 0);
@@ -173,17 +181,21 @@ let pCups = [
         z: -0.75,
         id: "blue11"
     }
-]
+] // Positions which we tested with the orbit controlls ui
 
 let now = Date.now();
-let deltaTime = 0.018;
+let deltaTime = 0.018; //fixed assumend framerate
 
 let mouseDownPos = new THREE.Vector2();
 let mouseUpPos = new THREE.Vector2();
 
-
 let cups = [];
 
+///////////////////////////////////////////////////////////////////////
+/*///////////////////////////// Functions ////////////////////////// */
+///////////////////////////////////////////////////////////////////////
+ 
+///////////// functions to set up the scene
 
 function addCups() {
     let i = 0
@@ -243,6 +255,74 @@ function addBall() {
     ball.updatePosition();
 }
 
+function rotate(e) {
+    let el = document.querySelector('.cups.' + this)
+    let currentRotation = el.getAttribute('rotation')
+
+    if (currentRotation == '0') {
+        el.style.transform = 'rotate(180deg)'
+        el.setAttribute('rotation', '180')
+    } else {
+        el.style.transform = 'rotate(0deg)'
+        el.setAttribute('rotation', '0')
+    }
+}
+
+// Adding Drunk Shader Effekt on the Ball. Adding fluctuate with Vertex Shader and color-Effekt with Fragement Shader. Based on how many cups were hit.
+function drunkShaderMaterial() {
+    document.getElementById("ball").object3D.children[0].material = new THREE.ShaderMaterial( {
+	        uniforms: {
+	        	tDiffuse: { value: null },
+                drunkStage: {type: 'float', value: drunkStage },
+                drunk: { type: 'bool', value: drunk },
+                u_Time: { value: 0.0},
+	        },
+        
+	        vertexShader: `
+            uniform bool drunk;
+            uniform float drunkStage;
+            uniform float u_Time;
+
+            varying vec2 vUv;
+
+            void main() {
+                float time = u_Time / 1000.0;
+                vUv = uv;
+                if(drunk) {
+                    gl_Position = projectionMatrix * modelViewMatrix * vec4( vec3(position[0] + sin(time) * drunkStage, position[1], position[2]), 1.0 );
+                } else {
+                    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+                }
+            }`,
+	        fragmentShader: 
+            `
+                uniform float u_Time;
+                uniform bool drunk;
+                uniform float drunkStage;
+
+                varying vec2 vUv;
+                
+                void main()
+                {
+                    if(drunk) {
+                        float time = u_Time/1000.0;
+                        vec3 col = vec3(1.0 - abs(drunkStage * sin(time * (4.0 * drunkStage * 10.0))), 1.0 - abs(drunkStage * sin(2.0 * time * (drunkStage * 10.0)) + 0.25), 1.0 - abs(drunkStage * sin(time * (drunkStage * 10.0)) + 0.5));
+
+                        //vec3 col = 0.5 + 0.5*cos(time*(drunkStage*10.0)+vUv.xyx+vec3(0,2,4));
+                        gl_FragColor = vec4(col,1.0);
+                    } else {
+                        gl_FragColor = vec4(1.0, 1.0, 1.0 ,1.0);
+                    }
+                }
+        
+            `
+        }
+        );
+}
+
+
+///////////// functions to controll Runtime
+
 function calculateDeltaTime() {
     deltaTime = (Date.now() - now) / 1000;
     now = Date.now();
@@ -280,6 +360,8 @@ function calculateCameraPosition() {
     Camera.instance.position = camPos;
 }
 
+///////////// Game Loop
+
 function animate() {
     calculateDeltaTime();
     calculateCameraPosition();
@@ -296,31 +378,29 @@ function animate() {
 
     ScaleEntity.instance.setAttribute("scale", "" + scale + " " + scale + " " + scale);
 
+    document.getElementById("ball").object3D.children[0].material.uniforms.u_Time.value = now - startTime;
+    //TODO: Update drunk value of Shader 
+    //TODO: Update drunkStage of Shader
+
     requestAnimationFrame(animate);
 }
 
 
-init();
-
-
-function rotate(e) {
-    let el = document.querySelector('.cups.' + this)
-    let currentRotation = el.getAttribute('rotation')
-
-    if (currentRotation == '0') {
-        el.style.transform = 'rotate(180deg)'
-        el.setAttribute('rotation', '180')
-    } else {
-        el.style.transform = 'rotate(0deg)'
-        el.setAttribute('rotation', '0')
-    }
-}
+///////////// UI Functions
 
 function toggleSettings() {
     let settings = document.querySelector('.settings')
     settings.classList.toggle('active')
 }
 
+function onSizeToggleChanged(change) {
+    scale = this.checked ? 4.5 : 1;
+}
+
+
+///////////// Instantiate everything
+
+init();
 function init() {
     Camera.instance = document.querySelector("a-entity[camera]");
     Camera.instance.setAttribute("rotation-position-reader");
@@ -331,6 +411,8 @@ function init() {
     addBall();
 
     addCups();
+
+    drunkShaderMaterial()
 
     document.querySelector('.openClose').addEventListener('click', toggleSettings)
     document.querySelector('.cups.blue').addEventListener('click', rotate.bind('blue'))
@@ -345,8 +427,4 @@ function init() {
     }
 
     animate();
-}
-
-function onSizeToggleChanged(change) {
-    scale = this.checked ? 4.5 : 1;
 }
